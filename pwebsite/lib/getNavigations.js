640202a7a2c88e7bdb6db6b6fb7852c34260127e
zang <keping.zang@vonaffenfels.de>
2021-02-26 11:46:15 +0100

    fixed bug

--- pwebsite/lib/getNavigations.js
+++ pwebsite/lib/getNavigations.js
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import path from 'path'
 
-const navigationDirectory = path.join(process.cwd(), 'pages\\navigations')
+const navigationDirectory = path.join(process.cwd(), 'pages/navigations')
 
 export function getNavigations() {
     const fileNames = fs.readdirSync(navigationDirectory)
